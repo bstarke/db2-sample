c93ffe0bfc5076ae6bb02c5fcaaeff49a0a607c2
Brad Starkenberg <brad@starkenberg.net>
2018-10-17 06:52:39 -0500

    Changes for config server

--- src/main/java/io/pivotal/examples/db2sample/Db2SampleApplication.java
+++ src/main/java/io/pivotal/examples/db2sample/Db2SampleApplication.java
@@ -1,15 +1,14 @@
 package io.pivotal.examples.db2sample;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class Db2SampleApplication {
 
-	public static void main(String[] args) {
-		log.debug("Starting Application");
-		SpringApplication.run(Db2SampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Db2SampleApplication.class, args);
+    }
 }
--- src/test/java/io/pivotal/examples/db2sample/SpringDataRestTest.java
+++ src/test/java/io/pivotal/examples/db2sample/SpringDataRestTest.java
@@ -2,25 +2,27 @@ package io.pivotal.examples.db2sample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@DirtiesContext
 public class SpringDataRestTest {
 
-    @Value("${local.server.port}")
+    @LocalServerPort
     private int port;
 
     @Test
@@ -34,40 +36,4 @@ public class SpringDataRestTest {
         assertEquals(new Double(94250), entity.getBody().getSalary());
     }
 
-/*    @Test
-    public void testGuardian() {
-        ResponseEntity<Guardian> entity = new TestRestTemplate().getForEntity(
-                "http://localhost:" + this.port + "/guardians/1", Guardian.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals("fred", entity.getBody().getFirstName());
-        assertEquals("flintstone", entity.getBody().getLastName());
-        assertEquals("fred@flintstone.net", entity.getBody().getEmail());
-        assertEquals(new Integer(12345), entity.getBody().getZipCode());
-        Assert.assertNotNull(entity.getBody().getCreateDate());
-        Assert.assertNotNull(entity.getBody().getLastModifiedDate());
-    }*/
-
-/*    @Test
-    public void testChild() {
-        ResponseEntity<Child> entity = new TestRestTemplate().getForEntity(
-                "http://localhost:" + this.port + "/children/1", Child.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals("pebbles", entity.getBody().getName());
-        assertEquals(false, entity.getBody().getSpecialNeeds());
-        assertEquals(LocalDate.of(2015, 1, 1), entity.getBody().getBirthDate());
-        Assert.assertNotNull(entity.getBody().getCreateDate());
-        Assert.assertNotNull(entity.getBody().getLastModifiedDate());
-    }*/
-
-/*    @Test
-    public void testChild2() {
-        ResponseEntity<Child> entity = new TestRestTemplate().getForEntity(
-                "http://localhost:" + this.port + "/children/2", Child.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals("bam", entity.getBody().getName());
-        assertEquals(true, entity.getBody().getSpecialNeeds());
-        assertEquals(LocalDate.of(2015, 11, 11), entity.getBody().getBirthDate());
-        Assert.assertNotNull(entity.getBody().getCreateDate());
-        Assert.assertNotNull(entity.getBody().getLastModifiedDate());
-    }*/
 }
